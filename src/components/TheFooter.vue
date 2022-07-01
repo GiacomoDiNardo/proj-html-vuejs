7ec4da5d7f2cce90244e580e23889babe84ed0c5
GiacomoDiNardo <vest_95@hotmail.it>
2022-07-01 17:09:15 +0200

    create basic footer layout and fix little style problems

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,13 +1,71 @@
 <template>
-  <div></div>
+  <div class="container">
+    <div class="row gap-5">
+        <div class="col col-3 me-5">
+            <!-- logo + testo -->
+            <img :src="footerLogo" alt="">
+
+            <div class="footer-text">It is a long established fact that a reader will be distracted by the readable content.</div>
+            <span>Copyright @ 2018 Sofbox</span>
+        </div>
+        <div class="col col-2">
+            <!-- Menù + ul -->
+            <h5>Menu</h5>
+            <ul>
+                <li v-for="link in linksList" :key="link"><a href="#">{{ link }}</a></li>
+            </ul>
+        </div>
+        <div class="col col-2">
+            <!-- About us + ul -->
+            <h5>About Us</h5>
+            <ul>
+                <li v-for="link in aboutUsLinks" :key="link"><a href="#">{{ link }}</a></li>
+            </ul>
+        </div>
+        <div class="col col-2">
+            <!-- Quick links + ul -->
+            <h5>Quick Links</h5>
+            <ul>
+                <li v-for="link in linksList" :key="link"><a href="#">{{ link }}</a></li>
+            </ul>
+        </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
+    data() {
+        return {
+            footerLogo: "/img/35.png",
+            linksList: [
+                "About us",
+                "Theme",
+                "Features",
+                "Pricing",
+                "Blog",
+            ],
+            aboutUsLinks: [
+                "About Sofbox",
+                "Roadmap",
+                "How it work",
+                "Team",
+                "News",
+            ]
+        }
+    }
 
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+.col {
+    ul {
+        list-style: none;
+        text-align: start;
+        padding-left: 0;
+    }
+}
+
 
 </style>
