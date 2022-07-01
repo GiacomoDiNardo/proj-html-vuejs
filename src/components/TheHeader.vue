2e1be9e67e5d0588f554c85b394df9e982ceadb0
GiacomoDiNardo <vest_95@hotmail.it>
2022-07-01 17:08:15 +0200

    create cart counter

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -4,15 +4,18 @@
         <div class="logo">
             <img :src="logoImg" alt="">
         </div>
-        <div class="header-nav ms-auto d-flex gap-5 align-items-center">
+        <div class="header-nav ms-auto d-flex gap-5 align-items-baseline">
             <div class="pages-nav">
-                <ul class="d-flex list-unstyled gap-4 align-items-baseline">
+                <ul class="d-flex list-unstyled gap-4">
                     <li v-for="(page, i) in pagesList" :key="i"><a href="#">{{ page }}</a></li>
                 </ul>
             </div>
-            <div class="nav-icons d-flex gap-3">
-                <i class="fa-solid fa-magnifying-glass"></i>
-                <i class="fa-solid fa-cart-shopping"></i>
+            <div class="nav-icons d-flex gap-4">
+                <a href="#"><i class="fa-solid fa-magnifying-glass"></i></a>
+                <div class="position-relative cart">
+                    <div class="position-absolute cart-counter">0</div>
+                    <a href="#"><i class="fa-solid fa-cart-shopping position-relative"></i></a>
+                </div>
             </div>
             <button class="btn btn-primary p-3">Get Started</button>
         </div>
@@ -32,7 +35,6 @@ export default {
                 "Feauture",
                 "Testimonials",
                 "Contact us",
-
             ]
         }
     }
@@ -41,18 +43,28 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "../assets/scss/variables";
 .logo {
     img{
         width: 40px;
     }
 }
 
-.pages-nav {
-    li {
-        a {
-            color: black;
-            text-decoration: none;
-        }
+.cart {
+    .cart-counter {
+        font-size: 12px;
+        background-color: $primary;
+        border-radius: 50%;
+        color: white;
+        width: 20px;
+        height: 20px;
+        transform: translate(50%, -35%);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    .fa-cart-shopping {
+        z-index: 1;
     }
 }
 </style>
