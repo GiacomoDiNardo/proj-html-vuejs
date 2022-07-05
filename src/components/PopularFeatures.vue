bc967eef7014eaf47a3e0438c093a05f0902a939
GiacomoDiNardo <vest_95@hotmail.it>
2022-07-05 17:55:38 +0200

    add style to popular feautures and tips sections

--- src/components/PopularFeatures.vue
+++ src/components/PopularFeatures.vue
@@ -1,25 +1,30 @@
 <template>
-  <div class="container py-5">
-    <div class="row">
-      <div class="col col-6 flex-grow-1">
-        <h2>Get tips tricks on how to skyrocket you sales.</h2>
-        <span>
-          Faff about only a quid blower I don't want no agro bleeding chimney<br>
-          pot burke tosser cras nice one boot fanny.!
-        </span>
-      </div>
-      <div class="col">
-        <button class="btn btn-primary">Explore More Feature</button>
-      </div>
-    </div>
+    <div class="position-relative">
+        <div class="bg-img position-absolute">
+            <img src="/img/14.png" alt="">
+        </div>
+        <div class="container py-5 position-relative">
+            <div class="row">
+                <div class="col col-6 flex-grow-1">
+                    <h2 class="pb-3">Popular feautures that your business needs</h2>
+                    <span class="d-inline-block text-grey pb-5">
+                        Faff about only a quid blower I don't want no agro bleeding chimney<br>
+                        pot burke tosser cras nice one boot fanny.!
+                    </span>
+                </div>
+                <div class="col align-self-center">
+                    <button class="btn btn-primary py-3 px-4">Explore More Feature</button>
+                </div>
+            </div>
 
-    <div class="row g-3">
-        <div class="col col-3" v-for="feature in featuresList" :key="feature.title">
-            <h5>{{ feature.title }}</h5>
-            <img :src="feature.src" :alt="feature.title + ' logo'">
+            <div class="row row-cols-4 g-3 gap-3">
+                <div class="col shadow" v-for="feature in featuresList" :key="feature.title">
+                    <h5>{{ feature.title }}</h5>
+                    <img :src="feature.src" :alt="feature.title + ' logo'">
+                </div>
+            </div>
         </div>
     </div>
-  </div>
 </template>
 
 <script>
@@ -65,5 +70,24 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss" scoped>
+@import "../assets/scss/variables";
+
+.container {
+    z-index: 1;
+}
+.bg-img {
+    top: 0;
+    left: 0;
+    transform: translate(-80%, -10%);
+
+    img {
+        width: 100%;
+    }
+}
+
+.shadow {
+    box-shadow: 0 0 10px $border-grey;
+    width: calc(100% / 4 - 1rem);
+}
 </style>
--- src/components/TipsSection.vue
+++ src/components/TipsSection.vue
@@ -2,16 +2,16 @@
   <div class="container py-5">
     <div class="row">
         <div class="col col-6">
-            <h2>Get tips tricks on how to skyrocket you sales.</h2>
-            <span>
+            <h2 class="pb-3">Get tips tricks on how to skyrocket you sales.</h2>
+            <span class="d-inline-block pb-5 text-grey">
                 Faff about only a quid blower I don't want no agro bleeding chimney pot
                 burke tosser cras nice one boot fanny.!
             </span>
-            <div class="my-card d-flex">
+            <div class="my-card d-flex gap-3 border-grey">
                 <img src="/img/510.png" alt="">
                 <div class="text">
-                    <h5>Reporting analysis</h5>
-                    <span>
+                    <h5 class="text-primary">Reporting analysis</h5>
+                    <span class="text-grey">
                         It is a long established fact that a reader will be distracted
                         by the readable content of a page when looking at its
                         layout.
@@ -19,11 +19,11 @@
                 </div>
             </div>
 
-            <div class="my-card d-flex">
+            <div class="my-card d-flex gap-3">
                 <img src="/img/511.png" alt="">
                 <div class="text">
                     <h5>Technical SEO Audit</h5>
-                    <span>
+                    <span class="text-grey">
                         It is a long established fact that a reader will be distracted
                         by the readable content of a page when looking at its
                         layout.
@@ -44,6 +44,22 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+@import "../assets/scss/variables";
+.my-card {
+    padding: 2rem .5rem;
+    
+    
 
+    img {
+        width: 70px;
+        height: 70px;
+        align-self: center;
+    }
+}
+
+.border-grey {
+    border: 1px solid $border-grey;
+    box-shadow: 10px 10px 30px $border-grey;
+}
 </style>
