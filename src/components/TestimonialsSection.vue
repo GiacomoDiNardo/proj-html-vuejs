790993dbae300f14b23bc3d9ea32033db0a7e7f5
GiacomoDiNardo <vest_95@hotmail.it>
2022-07-06 14:19:22 +0200

    add some style to testimonials section

--- src/components/TestimonialsSection.vue
+++ src/components/TestimonialsSection.vue
@@ -2,28 +2,28 @@
   <div class="container py-5">
     <div class="text-center">
         <h2>Our Testimonials</h2>
-        <span>If you are planning on developing a product landing app or website, take a look at this beautiful-crafted</span>
+        <span class="text-grey pb-5 d-inline-block">If you are planning on developing a product landing app or website,<br> take a look at this beautiful-crafted</span>
     </div>
     
-    <div class="row">
-        <div class="col col-5">
-            <div class="card">
-                <img src="/img/26.jpeg" alt="">
+    <div class="row justify-content-center">
+        <div class="col col-6">
+            <div class="card p-3 text-center">
+                <img class="m-auto my-4" src="/img/26.jpeg" alt="">
                 <h5>Julia Aann</h5>
-                <span>- COO Sofbox</span>
-                <p>
+                <span class="text-grey pb-4">- COO Sofbox</span>
+                <p class="text-grey text-start">
                     At vero eos et accusamus et iusto odio dignissimos
                     ducimus qui blanditiis praesentium vero eos et consectetur
                     accus adip adip.
                 </p>
             </div>
         </div>
-        <div class="col col-5">
-            <div class="card">
-                <img src="/img/27.jpeg" alt="">
+        <div class="col col-6">
+            <div class="card p-3 text-center">
+                <img class="m-auto my-4" src="/img/27.jpeg" alt="">
                 <h5>Mac Znder</h5>
-                <span>- Manager Sofbox</span>
-                <p>
+                <span class="text-grey pb-4">- Manager Sofbox</span>
+                <p class="text-grey text-start">
                     At vero eos et accusamus et iusto odio dignissimos
                     ducimus qui blanditiis praesentium vero eos et consectetur
                     accus adip adip.
@@ -40,6 +40,20 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+@import "../assets/scss/variables";
 
+.card {
+    
+    img {
+        width: 70px;
+        border-radius: 50%;
+        border: 1px solid white;
+        box-shadow:  0 0 10px $border-grey;
+    }
+
+    h5, span {
+        display: inline-block;
+    }
+}
 </style>
